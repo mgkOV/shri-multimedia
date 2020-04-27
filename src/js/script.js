8c95744cdf1c6a7a6ee7e13eafbe227a5eae2654
mgkOV <myagkov.mww@gmail.com>
2020-04-27 22:05:44 +0300

    add controls

--- src/js/script.js
+++ src/js/script.js
@@ -2,14 +2,37 @@ const playerList = document.querySelectorAll(".video-wrapper");
 
 playerList.forEach((p) => {
   p.addEventListener("click", handlePlayerClick);
-  const minimizeBtn = p.querySelector(".minimize-btn");
 });
 
 function handlePlayerClick(e) {
   this.classList.add("video-wrapper__fullscreen");
-  const minimizeBtn = e.target.closest(".minimize-btn");
+  const minimizeBtn = e.target.closest(".btn__minimize");
+  const settingsBtn = e.target.closest(".btn__settings");
+  const volumeBtn = e.target.closest(".btn__volume");
+  const video = this.querySelector(".video");
+  const settings = this.querySelector(".settings");
 
-  if (!minimizeBtn) return;
+  // MINIMIZE
+  if (minimizeBtn) {
+    video.muted = true;
+    this.classList.remove("video-wrapper__fullscreen");
+    settings.classList.remove("settings__visible");
+  }
 
-  this.classList.remove("video-wrapper__fullscreen");
+  // MUTE
+  if (volumeBtn) {
+    video.muted = !video.muted;
+    const volumeIcon = volumeBtn.querySelector(".btn_icon");
+
+    if (video.muted) {
+      volumeIcon.src = "assets/img/muted.svg";
+    } else {
+      volumeIcon.src = "assets/img/volume.svg";
+    }
+  }
+
+  // CONTRAST & BRIGHTNESS
+  if (settingsBtn) {
+    settings.classList.toggle("settings__visible");
+  }
 }
