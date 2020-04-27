60cdcd4bb6157aceaff2e4ded492b0c34ff4d711
mgkOV <myagkov.mww@gmail.com>
2020-04-27 17:46:33 +0300

    add minimize button

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,15 @@
 const playerList = document.querySelectorAll(".video-wrapper");
 
 playerList.forEach((p) => {
-  p.addEventListener("click", handleClick);
+  p.addEventListener("click", handlePlayerClick);
+  const minimizeBtn = p.querySelector(".minimize-btn");
 });
 
-function handleClick(e) {
+function handlePlayerClick(e) {
   this.classList.add("video-wrapper__fullscreen");
-  console.log(this);
+  const minimizeBtn = e.target.closest(".minimize-btn");
+
+  if (!minimizeBtn) return;
+
+  this.classList.remove("video-wrapper__fullscreen");
 }
