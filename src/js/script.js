519830cf0526c3d5cc27c6a914771e954d8567c5
mgkOV <myagkov.mww@gmail.com>
2020-04-28 14:55:28 +0300

    add comments to js

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,9 @@
+// Создаем контекст
 let audioCtx = initContext();
 
 const playerList = document.querySelectorAll(".video-wrapper");
 
+// Регистриуем обработчики событий
 playerList.forEach((p) => {
   p.addEventListener("click", handlePlayerClick);
   console.log(p);
@@ -11,6 +13,7 @@ playerList.forEach((p) => {
   const video = p.querySelector(".video");
   video.style.filter = "brightness(1) contrast(1)";
 
+  // Регистрируем яркость контраст
   contrast.addEventListener("input", (e) => {
     const filters = video.style.filter.split(" ");
     video.style.filter = `${filters[0]} contrast(${e.target.value})`;
@@ -22,6 +25,7 @@ playerList.forEach((p) => {
     video.style.filter = `brightness(${e.target.value}) ${filters[1]}`;
   });
 
+  // Создаем анализатор аудио
   let analyser = createAnalizer(audioCtx);
   const source = audioCtx.createMediaElementSource(video);
   source.connect(analyser);
@@ -40,6 +44,7 @@ playerList.forEach((p) => {
   }, 100);
 });
 
+// Обработчик кликов
 function handlePlayerClick(e) {
   this.classList.add("video-wrapper__fullscreen");
   const minimizeBtn = e.target.closest(".btn__minimize");
@@ -56,7 +61,7 @@ function handlePlayerClick(e) {
     settings.classList.remove("settings__visible");
   }
 
-  // MUTE
+  // MUTE/UNMUTE
   if (volumeBtn) {
     video.muted = !video.muted;
     const volumeIcon = volumeBtn.querySelector(".btn_icon");
@@ -72,22 +77,23 @@ function handlePlayerClick(e) {
     }
   }
 
-  // CONTRAST & BRIGHTNESS
+  // Переключаем отоброжение панели Яркость/Контраст
   if (settingsBtn) {
     settings.classList.toggle("settings__visible");
   }
 }
 
+// Инициализатор аудио контекста
 function initContext() {
   const AudioContext = window.AudioContext || window.webkitAudioContext;
   return new AudioContext();
 }
 
-function createAnalizer(context) {
+function createAnalizer(context, fftSize = 32, timeConstant = 0) {
   const analyser = context.createAnalyser();
 
-  analyser.fftSize = 32;
-  analyser.smoothingTimeConstant = 0;
+  analyser.fftSize = fftSize;
+  analyser.smoothingTimeConstant = timeConstant;
 
   return analyser;
 }
